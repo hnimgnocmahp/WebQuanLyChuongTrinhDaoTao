35cde3b2c0c4ad67b822993790601b16ae15fd88
Nguyễn Trung Anh <147566984+Anhdeptroai@users.noreply.github.com>
2025-05-18 13:37:54 +0700

    update

--- src/main/java/com/example/quanlydaotao/Quanlydaotaoapplication.java
+++ src/main/java/com/example/quanlydaotao/Quanlydaotaoapplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Quanlydaotaoapplication {
+public class uanlydaotaoapplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Quanlydaotaoapplication.class, args);
