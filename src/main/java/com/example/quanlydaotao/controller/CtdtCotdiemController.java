0f4851d603fe002baa6dd7d69c92e71f1319269f
Nguyễn Trung Anh <147566984+Anhdeptroai@users.noreply.github.com>
2025-05-20 20:11:11 +0700

    update

--- src/main/java/com/example/quanlydaotao/controller/CtdtCotdiemController.java
+++ src/main/java/com/example/quanlydaotao/controller/CtdtCotdiemController.java
@@ -1,5 +1,14 @@
 package com.example.quanlydaotao.controller;
 
+import com.example.quanlydaotao.entity.CtdtCotdiem;
+import com.example.quanlydaotao.repository.CtdtCotdiemRepository;
+import com.example.quanlydaotao.repository.CtdtDecuongchitietRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import java.math.BigDecimal;
 import java.util.List;
 
